c1d63bde643fb1a86acd329855d2f8b94c839e77
nij-patel <nijpatel28@gmail.com>
2024-02-29 01:47:24 -0500

    created the calculator app

---
+++ Calculator.java
@@ -0,0 +1,17 @@
+// Simple integer calculator 
+
+public class Calculator {
+
+    static int add(int a, int b) {
+        return a + b;
+    }
+
+    // TODO: Create subtract method
+
+    // TODO: Create divide method
+
+    // TODO: Create exponent method
+
+    // TODO: Create
+
+}
